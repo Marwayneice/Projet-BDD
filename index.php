486df8a20bd4b7ac982e3792e2f9e425f44b5557
kennyguiroy123 <kenny.guiroy@epsi.fr>
2017-10-04 15:29:20 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8" />
+	<link rel="stylesheet" href="bootstrap/css/bootstrap.min.css"/>
+	<link rel="stylesheet" type="text/css" href="Test.css">
+	<title> Test </title>
+</head>
+<body>
+	<div>
+   		 <div class="block">
+			<div class="btn-group btn-group-justified">
+            	<div class="btn-group">
+                	<button type="button" class="btn btn-nav">
+                   	 <span><img src="acceuil.jpg" class="iconacceuil"></span>
+    			   	 </a><p>Acceuil</p>
+               		 </button>
+           		 </div>
+            <div class="btn-group">
+                <button type="button" class="btn btn-nav">
+                    <span class="#"></span>
+    			    <p>Résérvation</p>
+                </button>
+            </div>
+            <div class="btn-group">
+                <button type="button" class="btn btn-nav">
+                    <span class="#"></span>
+    			    <p>Evénements</p>
+                </button>
+            </div>
+            <div class="btn-group">
+                <button type="button" class="btn btn-nav">
+                    <span class="#"></span>
+    			    <p>Mon Compte</p>
+                </button>
+            </div>
+        </div>
+	</div>
+</div>
+
+</body>
+</html>
